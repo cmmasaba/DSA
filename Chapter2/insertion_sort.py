76196c7b9b80ee94f59c4a8f2b5bd48a45fdb30c
Collins Mmasaba <mmasabacollins9@gmail.com>
2023-11-01 14:52:17 +0300

    Insertion sort implemented using Python

---
+++ Chapter2/insertion_sort.py
@@ -0,0 +1,33 @@
+"""Insertion sort program implemented using Python"""
+
+def insertion_sort(array: list):
+    key: int = 0
+
+    for i in range(1, len(array)):
+        key = array[i]
+        j:int = i - 1
+
+        while j>= 0 and array[j] > key:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = key
+
+def print_array(array: list):
+    for item in array:
+        print(item, end=" ")
+    print()
+
+def main():
+    print("This program sorts an array in ascending order using"
+          " Insertion Sort algorithm.\n")
+    array = input("Enter the elements of the array: ")
+    print()
+    array = list(map(int, array.split()))
+    print(f"The unsorted array is:")
+    print_array(array)
+    insertion_sort(array)
+    print(f"The sorted array is:")
+    print_array(array)
+
+if __name__ == "__main__":
+    main()
