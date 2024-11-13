745084c1fb5124dda45ecdb77bfe29a4b10013c2
cmmasaba <mmasabacollins9@gmail.com>
2024-11-13 11:52:42 +0300

    Replace spaces in a string with %20

--- algorithms/general/urlify.py
+++ algorithms/general/urlify.py
@@ -1,5 +1,9 @@
 """
-Write a method that replaces all spaces in a string with %20. You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the "true length" of the string.
+Write a method that replaces all spaces in a string with %20. 
+You may assume that the string has sufficient space at the end 
+to hold the additional characters, and that you are given the 
+"true length" of the string.
+
 """
 
 def urlify(string: str, length: int):
@@ -12,10 +16,22 @@ def urlify(string: str, length: int):
         A new string with the spaces replaced.
     """
     str_list = list(string)
-    spaces_count = str_list.count(" ")
+    spaces_count = 0
+    # Count the number of spaces
+    for i in range(length - 1, -1, -1):
+        if string[i] == ' ':
+            spaces_count += 1
 
-    for i in range(spaces_count):
-        idx = str_list.index(" ")
-        str_list[idx] = "%20"
+    index = length + spaces_count * 2
+    # Replace spaces with %20
+    for i in range(length - 1, -1, -1):
+        if str_list[i] == " ":
+            str_list[index - 1] = '0'
+            str_list[index - 2] = '2'
+            str_list[index - 3] = '%'
+            index -= 3
+        else:
+            str_list[index - 1] = str_list[i]
+            index -= 1
 
     return "".join(str_list)
