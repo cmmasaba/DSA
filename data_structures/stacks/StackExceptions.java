b4e528eaf79dfdaa66cb981f3c40b3a536d8ee03
cmmasaba <mmasabacollins9@gmail.com>
2024-11-20 11:27:52 +0300

    Exceptions for stack overflow and underflow

---
+++ data_structures/stacks/StackExceptions.java
@@ -0,0 +1,11 @@
+/**
+ * Exceptions for overflow and underflow in stacks
+ */
+
+
+ public class StackExceptions extends Exception {
+ 
+     public StackExceptions(String message) {
+         super(message);
+     }
+ }
