0b3032fbbab1d2335a7b2e17ed329593a90a4ef0
cmmasaba <mmasabacollins9@gmail.com>
2024-11-20 11:05:12 +0300

    Implementation of queues

--- data_structures/queues/Queue.java
+++ data_structures/queues/Queue.java
@@ -1,9 +1,12 @@
 /**
- * Implementation of a Queue data structure.
+ * Implementation of a Queue data structure of size n-1 using
+ * an array of size n as the underlying structure.
+ * The queue has operations like queue-empty, queue-full, enqueue and dequeue.
+ * And attributes like Q.tail, Q.size and Q.head.
  */
 
 package data_structures.queues;
 
 public class Queue{
-    
+
 }
