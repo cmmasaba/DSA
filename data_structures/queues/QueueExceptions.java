ac70cd930e9d946cceddbfeca7a85764a06b4593
cmmasaba <mmasabacollins9@gmail.com>
2024-11-20 11:16:57 +0300

    Exceptions for queue overflow or underflow

---
+++ data_structures/queues/QueueExceptions.java
@@ -0,0 +1,12 @@
+/**
+ * Exceptions for overflow and underflow in queues
+ */
+
+package data_structures.queues;
+
+public class QueueExceptions extends Exception {
+
+    public QueueExceptions(String message) {
+        super(message);
+    }
+}
