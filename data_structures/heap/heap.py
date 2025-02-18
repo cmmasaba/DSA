e9a2a7528087fdb6566f3d3b024735d75259cd65
cmmasaba <mmasabacollins9@gmail.com>
2025-02-18 08:07:58 +0300

    Heap data structure

--- data_structures/heap/heap.py
+++ data_structures/heap/heap.py
@@ -4,10 +4,14 @@ Implementation of a heap data structure
 
 class Heap:
 
-    def __init__(self, array: list):
+    def __init__(self, array: list, max=True):
         self.heap: list = array
         self.heap_size: int = len(self.heap)
-        self.build_max_heap()
+        self.max_heap = max
+        if not self.max_heap:
+            self.build_min_heap()
+        else:
+            self.build_max_heap()
     
     def parent(self, index):
         return index // 2
@@ -18,7 +22,7 @@ class Heap:
     def right(self, index):
         return (2 * index) + 1
     
-    def max_heapify(self, index = 0):
+    def max_heapify(self, index = 1):
         '''
         Maintain the max-heap property
 
@@ -28,19 +32,19 @@ class Heap:
         left = self.left(index)
         right = self.right(index)
 
-        if left <= self.heap_size and self.heap[left] > self.heap[index]:
+        if left <= self.heap_size and self.heap[left - 1] > self.heap[index - 1]:
             largest = left
         else:
             largest = index
         
-        if right <= self.heap_size and self.heap[right] > self.heap[index]:
+        if right <= self.heap_size and self.heap[right - 1] > self.heap[largest - 1]:
             largest = right
         
         if largest != index:
-            self.heap[index], self.heap[largest] = self.heap[largest], self.heap[index]
+            self.heap[index - 1], self.heap[largest - 1] = self.heap[largest - 1], self.heap[index - 1]
             self.max_heapify(largest)
 
-    def min_heapify(self, index = 0):
+    def min_heapify(self, index = 1):
         '''
         Maintain the min-heap property
 
@@ -50,28 +54,71 @@ class Heap:
         left = self.left(index)
         right = self.right(index)
 
-        if left <= self.heap_size and self.heap[left] < self.heap[index]:
+        if left <= self.heap_size and self.heap[left - 1] < self.heap[index - 1]:
             smallest = left
         else:
             smallest = index
         
-        if right <= self.heap_size and self.heap[right] < self.heap[index]:
+        if right <= self.heap_size and self.heap[right - 1] < self.heap[smallest - 1]:
             smallest = right
         
         if smallest != index:
-            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+            self.heap[index - 1], self.heap[smallest - 1] = self.heap[smallest - 1], self.heap[index - 1]
             self.min_heapify(smallest)
     
     def build_max_heap(self):
         '''
         Build a max heap from a list of elements.
         '''
-        for index in range(len(self.heap) // 2, 1, -1):
+        n = len(self.heap)
+        for index in range((n // 2), 0, -1):
             self.max_heapify(index)
     
     def build_min_heap(self):
         '''
         Build a min heap from a list of elements.
         '''
-        for index in range(len(self.heap) // 2, 1, -1):
+        n = len(self.heap)
+        for index in range((n // 2), 0, -1):
             self.min_heapify(index)
+
+
+    def heapsort(self):
+        if self.max_heap:
+            self.max_heap_sort()
+        else:
+            self.min_heap_sort()
+
+    def max_heap_sort(self):
+        self.build_max_heap()
+        n = self.heap_size
+        for index in range(n, 1, -1):
+            self.heap[0], self.heap[index - 1] = self.heap[index - 1], self.heap[0]
+            self.heap_size = self.heap_size - 1
+            self.max_heapify(1)
+
+    def min_heap_sort(self):
+        self.build_min_heap()
+        n = self.heap_size
+        for index in range(n, 1, -1):
+            self.heap[0], self.heap[index - 1] = self.heap[index - 1], self.heap[0]
+            self.heap_size = self.heap_size - 1
+            self.min_heapify(1)
+
+    def print_heap(self):
+        n = len(self.heap)
+        for index in range(len(self.heap)):
+            print(self.heap[index], end=', ' if index != n-1 else '\n')
+        print()
+
+
+def main():
+    array = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
+    heap = Heap(array, max=False)
+    heap.print_heap()
+    heap.heapsort()
+    heap.print_heap()
+
+
+if __name__ == "__main__":
+    main()
