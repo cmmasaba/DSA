98f6527b3be207730d9e00b20b99dac8e1b8a471
cmmasaba <mmasabacollins9@gmail.com>
2025-03-31 14:07:30 +0300

    Palindrome permetation

---
+++ algorithms/general/palindrome_permutation.py
@@ -0,0 +1,10 @@
+"""Given a string, write a function to check if it is
+a permutation of a palindrome. A palindrome is a word
+that is the same forwards and backwards. A permutation
+is a rearrangement of letters.
+Example:
+Input: Tact Toa
+Output: True(permutations: taco cat, atco cta, etc)
+
+
+"""
