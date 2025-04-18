cfdab5a5f1545635a4b9c998759b62756aefeeee
cmmasaba <mmasabacollins9@gmail.com>
2025-04-18 22:10:44 +0300

    Longest substring

--- algorithms/general/longest_unique_substring.py
+++ algorithms/general/longest_unique_substring.py
@@ -1,11 +1,6 @@
 import time
 
-class Solution:    
-    def allSubstrings(self, s):
-        for i in range(len(s)):
-            for j in range(i, len(s)):
-                yield s[i: j+1]
-
+class Solution:
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
