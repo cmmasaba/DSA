f03329c8102210569ffeb6b4dd1d41d568c45fe2
cmmasaba <mmasabacollins9@gmail.com>
2025-04-18 21:55:20 +0300

    Remove duplicate elements from array such that each element appears at most twice

---
+++ algorithms/general/remove_duplicates2.py
@@ -0,0 +1,39 @@
+"""
+Given an integer array nums sorted in non-decreasing order,
+remove some duplicates in-place such that each unique element
+appears at most twice. The relative order of the elements
+should be kept the same.
+
+Since it is impossible to change the length of the array in
+some languages, you must instead have the result be placed
+in the first part of the array nums. More formally, if there
+are k elements after removing the duplicates, then the first
+k elements of nums should hold the final result. It does not
+matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+"""
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return None
+
+        i = 1
+        j = 1
+        count = 1
+
+        while i < len(nums):
+            if nums[i] == nums[i - 1] and j < 2:
+                nums[count] = nums[i]
+                count += 1
+                j += 1
+            elif nums[i] != nums[i - 1]:
+                nums[count] = nums[i]
+                count += 1
+                j = 1
+            i += 1
+        return count
