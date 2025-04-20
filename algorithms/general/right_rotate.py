37072de761fe5a2ba9397293867714e28955f7f3
cmmasaba <mmasabacollins9@gmail.com>
2025-04-20 22:36:56 +0300

    Rotate an array to the right by n positions

---
+++ algorithms/general/right_rotate.py
@@ -0,0 +1,28 @@
+"""
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative
+"""
+class Solution(object):
+    def reverse(self, nums, start, end):
+        """
+        :type nums: List[int]
+        """
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k = k % n # Handle cases when k > n
+        self.reverse(nums, 0, len(nums) - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, len(nums) - 1)
+
+if __name__ == "__main__":
+    soln = Solution()
+    s = [1, 2, 3, 4, 5, 6, 7]
+    soln.rotate(s, 3)
+    print(s)
